37f61b6d5c9e69ce8e8be825be76cc7ebdb01050
anh627 <linhanhtung08@gmail.com>
2025-09-26 21:48:03 +0700

    Update constants.ts

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -1,5 +1,8 @@
-export const EMPTY = 0;
-export const BLACK = 1;
-export const WHITE = 2;
-export const BOARD_SIZES = [9, 13, 19] as const;
-export type BoardSize = typeof BOARD_SIZES[number];
+export const EMPTY: Player = 0;
+export const BLACK: Player = 1;
+export const WHITE: Player = 2;
+export const STAR_POINTS: Record<BoardSize, [number, number][]> = {
+  9: [[2, 2], [2, 6], [6, 2], [6, 6], [4, 4]],
+  13: [[3, 3], [3, 9], [9, 3], [9, 9], [6, 6]],
+  19: [[3, 3], [3, 15], [15, 3], [15, 15], [9, 9]],
+};
